54c495440e145affbd81a7568e277460b01c157a
Luan Rocha <luanrocha.ios@gmail.com>
2022-09-13 15:20:26 -0300

    Adicionaodo banco de dados sqlite

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.get('/', function(req, res){
     res.send("Ola mundo!")
 })
+
+app.post('/pessoa', function(req, res){
+    console.log(req.body);
+    res.json({
+        "statucCode": 200
+    })
+});
 
 app.listen(3000, ()=> console.log("Servidor rodando na porta 3000"))
